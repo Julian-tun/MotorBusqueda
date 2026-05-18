b8439ebd115d868b1dfde02cc127aa5d072cfcdb
Julian-tun <juliantunortiz@gmail.com>
2026-05-17 18:36:07 -0600

    Remove sensitive config from repository

--- config_mongo.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'uri' => 'mongodb+srv://juliantunortiz_db_user:LvUDo8TlnxQ4Sqd0@cluster0.fdqifn8.mongodb.net/motorbusqueda?retryWrites=true&w=majority',
-    'db' => 'motorbusqueda',
-    'collection' => 'resumenes_cache'
-];
